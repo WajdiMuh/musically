ca8a455239a7086452e095e07003e928c1519b22
Wajdimuh <w.elmuhtadi@gju.edu.jo>
2021-09-19 03:19:48 +0200

    begin adding music commands

--- musically.py
+++ musically.py
@@ -1,23 +1,37 @@
 import os
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '-')
+
+musicqueue = []
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user.display_name} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command(aliases=['p'])
+async def play(ctx,songname):
+    if len(musicqueue) == 0:
+        await ctx.send(f'playing {songname}')
+    else:
+        await ctx.send(f'{songname} added to queue')
+    musicqueue.append(songname)
 
-    if message.content.startswith('saif'):
-        await message.channel.send('gay')
+@client.command()
+async def clear(ctx):
+    del musicqueue[:]
+    await ctx.send(f'queue cleared')
+
+@client.command(aliases=['q'])
+async def queue(ctx):
+    if len(musicqueue) == 0:
+        await ctx.send(f'the queue is empty')
+    else:
+        await ctx.send("\n".join(musicqueue))
 
 client.run(TOKEN)
